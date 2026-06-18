cbc6b704d18bcf5d6cf512ebc66cef0d18cb3072
Erick Kahinga <106274320+erickkahinga@users.noreply.github.com>
2026-06-18 14:07:04 -0700

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+        if (!head) {
+            return head
+        }
+
+        let tempArr = [head.val];
+        let newHead = new ListNode("dummy");
+        let curr = head;
+
+        while (curr && curr.next) {
+            curr = curr.next;
+            tempArr.push(curr.val);
+        }
+
+        curr = newHead;
+
+        for (let i = tempArr.length - 1; i >= 0; i--) {
+            curr.next = new ListNode(tempArr[i]);
+            curr = curr.next
+        }
+
+        return newHead.next;
+    }
+}
